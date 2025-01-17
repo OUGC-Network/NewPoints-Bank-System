c06071b5446c9ba9b82a343d18069acab70d7d10
Sama34 <Sama34@users.noreply.github.com>
2025-01-16 16:59:07 -0800

    META 3.1.1

--- Upload/inc/plugins/newpoints/plugins/BankSystem/admin.php
+++ Upload/inc/plugins/newpoints/plugins/BankSystem/admin.php
@@ -247,8 +247,8 @@ function plugin_information(): array
         'website' => 'https://ougc.network',
         'author' => 'Omar G.',
         'authorsite' => 'https://ougc.network',
-        'version' => '3.1.0',
-        'versioncode' => 3100,
+        'version' => '3.1.1',
+        'versioncode' => 3101,
         'compatibility' => '31*',
         'codename' => 'newpoints_bank_system'
     ];
